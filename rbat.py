33abf8c11b3d544f25f37a99544e46588162413f
Marco Piccinini <mpicc@outlook.it>
2021-03-03 17:11:46 +0100

    New Proj

--- rbat.py
+++ rbat.py
@@ -1,6 +1,6 @@
 from spmf import Spmf
 
-spmf = Spmf("PrefixSpan", input_filename="/BMS1_spmf.txt",
+spmf = Spmf("PrefixSpan", input_filename="BMS1_spmf.txt",
             output_filename="output.txt", arguments=[0.7, 5])
 spmf.run()
 print(spmf.to_pandas_dataframe(pickle=True))
