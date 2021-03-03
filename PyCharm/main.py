e9f7c4e6cf0e656d9383ffaeab467de4c0ee087a
Marco Piccinini <mpicc@outlook.it>
2021-03-03 17:30:02 +0100

    Added spmf.jar

--- PyCharm/main.py
+++ PyCharm/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from spmf import Spmf
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+spmf = Spmf("PrefixSpan", input_filename="../BMS1_spmf.txt",
+            output_filename="output.txt", arguments=[0.7, 5])
+spmf.run()
+print(spmf.to_pandas_dataframe(pickle=True))
+spmf.to_csv("output.csv")
